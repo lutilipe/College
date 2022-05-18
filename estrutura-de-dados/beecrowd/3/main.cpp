39cae7324ed33869563c2ec5eb6f8c2a9e4676c9
lutilipe <lutilipe02@hotmail.com>
2022-05-17 23:32:28 -0300

    feat: add final submit

--- estrutura-de-dados/beecrowd/3/main.cpp
+++ estrutura-de-dados/beecrowd/3/main.cpp
@@ -78,30 +78,92 @@ int Queue::size() {
 class Tree {
     private:
         TreeNode* root;
+        void addHelper(TreeNode* &p, int val);
+        void delHelper(TreeNode* p);
     public:
         Tree() { root = NULL; };
+        ~Tree();
         void add(int val);
+        std::string bfs();
+        TreeNode* getRoot() { return root; };
 };
 
-void addHelper(TreeNode* root) {
-    if (!root) return;
-    
-}
-
-void Tree::add(int val) {
-    TreeNode *t = new TreeNode();
-    t->val = val;
-    if (root == NULL) {
-        root = t;
-        return;
+void Tree::delHelper(TreeNode *p){
+    if(p != NULL){
+        delHelper(p->left);
+        delHelper(p->right);
+        delete p;
     }
 }
 
-int main() {
+Tree::~Tree() {
+    delHelper(root);
+    root = NULL;
+}
+
+std::string Tree::bfs() {
     Queue *q = new Queue();
-    std::cout << q->size() << std::endl;
-    std::cout << q->dequeue() << std::endl;
-    std::cout << q->size() << std::endl;
+    std::string ans = "";
+    q->enqueue(root);
+    while (!q->empty()) {
+        TreeNode* tmp = q->dequeue();
+        if (tmp != NULL) {
+            ans += std::to_string(tmp->val);
+            ans += " ";
+            q->enqueue(tmp->left);
+            q->enqueue(tmp->right);
+        }
+        delete tmp;
+    }
     delete q;
+
+    ans.pop_back();
+    return ans;
+}
+
+void Tree::addHelper(TreeNode* &p, int val) {
+    if (p == NULL) {
+        p = new TreeNode();
+        p->val = val;
+    } else {
+        if (val > p->val) {
+            addHelper(p->right, val);
+        } else {
+            addHelper(p->left, val);
+        }
+    }
+}
+
+void Tree::add(int val) {
+    addHelper(root, val);
+}
+
+int main() {
+    int numberOfCases = 0, i = 0;
+    std::cin >> numberOfCases;
+
+    std::string ans = "";
+
+    for (i = 0; i < numberOfCases; i++) {
+        int treeLength = 0, j = 0;
+        std::cin >> treeLength;
+
+        Tree *t = new Tree();
+
+        ans += "Case ";
+        ans += std::to_string(i+1);
+        ans += ":";
+        ans += '\n';
+
+        for (j = 0; j < treeLength; j++) {
+            int tmp = 0;
+            std::cin >> tmp;
+            t->add(tmp);
+        }
+        ans += t->bfs();
+        ans += '\n';
+        ans += '\n';
+    }
+    std::cout << ans;
     return 0;
 }
