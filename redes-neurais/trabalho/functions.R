eec02f4e39c7a952e7826ad1e7d812a031d97426
lutilipe <lutilipe02@hotmail.com>
2022-12-01 21:14:27 -0300

    feat: add softmax functions

--- redes-neurais/trabalho/functions.R
+++ redes-neurais/trabalho/functions.R
@@ -5,3 +5,42 @@ softplus <- function(x, k=1) {
 dsoftplus <- function(x, k=1) {
   return(1/(1+exp(-k*x)))
 }
+
+softmax <- function(par){
+  n.par <- length(par)
+  par1 <- sort(par, decreasing = TRUE)
+  Lk <- par1[1]
+  for (k in 1:(n.par-1)) {
+    Lk <- max(par1[k+1], Lk) + log1p(exp(-abs(par1[k+1] - Lk))) 
+  }
+  val <- exp(par - Lk)
+  return(val)
+}
+
+matrixDiag <- function(m) {
+  newM <- matrix(0, ncol=ncol(m), nrow=ncol(m))
+  for (i in 1:ncol(m)) {
+    newM[i,][i] <- m[1,][i]
+  }
+  return(newM)
+}
+
+dsoftmax <- function(s){
+  jacobian_m <- matrixDiag(s)
+  
+  for (i in 1:nrow(jacobian_m)) {
+    for (j in 1:ncol(jacobian_m)) {
+      if (i == j) {
+        jacobian_m[i,][j] <- s[i] * (1-s[i])
+      } else {
+        jacobian_m[i,][j] <- -s[i]*s[j]
+      }
+    }
+  }
+  print(jacobian_m)
+  return(jacobian_m)
+  
+}
+
+softmax(matrix(c(1,2),nrow=1))
+dsoftmax(softmax(matrix(c(1,2),nrow=1)))
